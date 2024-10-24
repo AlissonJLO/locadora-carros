242315fafbf8f0cff0a7062b045144a807dfc4f1
Alisson Joaquim <alisson.oliveira@sou.ufmt.br>
2024-10-23 21:03:17 -0400

    feat:refactor docker

--- build.bat
+++ build.bat
@@ -1,15 +1,9 @@
 @echo off
 
-REM Etapa 1: Compilar o projeto localmente
-echo Building the Maven project locally...
-mvn clean package -DskipTests
-
-REM Verificar se o build foi bem sucedido
-IF %ERRORLEVEL% EQU 0 (
-    echo Build successful, preparing to create Docker image...
-
-    REM Etapa 2: Subir o contêiner do Docker usando o JAR gerado
-    docker-compose up --build
-) ELSE (
-    echo Build failed. Check errors.
-)
+REM Função para compilar a aplicação localmente com Maven
+echo.
+echo =============================================================================
+echo                          BUILDING APPLICATION
+echo =============================================================================
+echo.
+docker-compose up --build
